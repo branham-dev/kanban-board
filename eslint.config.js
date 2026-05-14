50bc244a946d2797ad8225c4e6757bbab70a6d0d
Branham Karanja <branham.karanja@outlook.com>
2026-05-14 17:59:19 +0300

    chore(root): adjust eslint rules and update gitignore

--- eslint.config.js
+++ eslint.config.js
@@ -27,7 +27,8 @@ export default defineConfig([
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/consistent-type-imports': 'error',
-      'no-console': 'warn',
+      // 'no-console': 'warn',
+      semi: ['error', 'always'],
     },
   },
 ]);
